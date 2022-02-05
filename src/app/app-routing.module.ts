a8ab8d1ea3cf50d20eca08ffed46c2a6428f6b88
OleHJoh <johansen.ole.h@gmail.com>
2022-02-05 17:15:09 +0100

    Fucking files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LoginPage } from "./pages/login/login.page";
+
+const routes: Routes = [
+    {
+        path: "",
+        component: LoginPage
+    }
+]
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemonListItemComponent } from './components/pokemon-list-item/pokemon-list-item.component';
+import { ProfilePage } from './pages/profile/profile.page';
+import { CataloguePage } from './pages/catalogue/catalogue.page';
+import { LoginPage } from './pages/login/login.page';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonListComponent,
+    PokemonListItemComponent,
+    ProfilePage,
+    CataloguePage,
+    LoginPage
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

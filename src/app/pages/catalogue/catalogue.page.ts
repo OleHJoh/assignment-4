37b5ccc5aeef200b052cbfd7b4a701745db56f24
OleHJoh <johansen.ole.h@gmail.com>
2022-02-10 01:38:22 +0100

    Deploy: Heroku Buildback

--- src/app/pages/catalogue/catalogue.page.ts
+++ src/app/pages/catalogue/catalogue.page.ts
@@ -7,10 +7,9 @@ import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-catalogue',
   templateUrl: './catalogue.page.html',
-  styleUrls: ['./catalogue.page.css']
+  styleUrls: ['./catalogue.page.css'],
 })
 export class CataloguePage implements OnInit {
-
   //Get function for type pokemon array
   get pokemons(): Pokemon[] {
     //Returns the pokemon array from pokemonService
@@ -22,22 +21,21 @@ export class CataloguePage implements OnInit {
     //Returns the username string from userService
     return this.userService.username;
   }
-//Constructs the different objects of type of other components in the application
+  //Constructs the different objects of type of other components in the application
   constructor(
     private userService: UserService,
-    private pokemonService: PokemonService) { }
+    private pokemonService: PokemonService
+  ) {}
 
-    //Initiate the function getUserData when component is loaded into the view or by an another component
+  //Initiate the function getUserData when component is loaded into the view or by an another component
   ngOnInit(): void {
     this.userService.getUserData();
   }
 
   //Function that gets activated when it receives a message from the child element
-  onCaptured(pokemon: Pokemon){
-    alert(pokemon.name + " was captured")
+  onCaptured(pokemon: Pokemon) {
+    alert(pokemon.name + ' was captured');
     this.userService.pokemons.push(pokemon.name);
     console.log(this.userService.user);
-    
   }
-
 }
